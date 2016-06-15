e8243162868a26ad23a69f1d01048c8d5c501f71
Heather <hdavidson94@gmail.com>
2016-06-15 16:37:54 +0100

    Add styling

--- stopwatchtests.js
+++ stopwatchtests.js
@@ -10,6 +10,17 @@ test('testing function is working', function(assert) {
   assert.equal(actual, expected, 'test environment is set up correctly')
 })
 
+test('start timer runs', function(assert) {
+    var done = assert.async();
+    var start_button = document.getElementById("start_button")
+      document.getElementById("start_button").addEventListener("click", function(){
+        var actual = document.getElementById("results").innerHTML;
+        var expected = '0.00';
+        assert.equal(actual, expected, 'the start timer runs')
+        done();
+      })
+})
+
 test('reset button injects HTML into results div', function(assert) {
   var done = assert.async();
   var reset_button = document.getElementById("reset_button")
