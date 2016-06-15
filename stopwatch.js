7707f7777e18749b8a4df11b79e4aba0b1538235
Heather <hdavidson94@gmail.com>
2016-06-15 15:11:22 +0100

    Start, stoop, reset buttons working

--- stopwatch.js
+++ stopwatch.js
@@ -1,37 +1,35 @@
+var i = 1;
+var timer;
+var divide = 100;
+var temp = false;
 
-function stopWatch(time) {
-  var i = 1;
-  var timer;
-  var divide = 100;
-  var tester;
 
-  // document.getElementById("start_button").addEventListener("click", function(){
-  //   console.log('clicked!');
-  //   document.getElementById("results").innerHTML = "The start button works";
-  // });
+
+function initialiseStop() {
   document.getElementById("stop_button").addEventListener("click", function(){
-    document.getElementById("results").innerHTML = "The stop button works";
+    clearInterval(timer);
   });
-  document.getElementById("reset_button").addEventListener("click", function(){
-    document.getElementById("results").innerHTML = "The reset button works";
-  });
-  document.getElementById("start_button").addEventListener("click", function(){
-    timer = setInterval(increment, 1000);
-    function increment() {
-      document.getElementById("results").innerHTML = i++;
-      tester = true;
-    }
-  });
-
-  // var i, timer, divide;
-  //   i = 0;
-  //   divide = 100;
-  //   function start() {
-  //       timer = setInterval('increment()',(1000 / divide)
-  //     }
-  //     function increment() {
-  //       i++;
-  //       document.getElementById('timer_out').innerHTML = (i / divide)
-  //     }
 }
-stopWatch()
+
+document.getElementById("start_button").addEventListener("click", function(){
+  temp = true;
+  timer = setInterval(increment, 100);
+  function increment() {
+    i++
+    document.getElementById("results").innerHTML = i / 100;
+  }
+});
+
+function initialiseReset() {
+  document.getElementById("reset_button").addEventListener("click", function(){
+    i = 0;
+    clearInterval(timer);
+    document.getElementById("results").innerHTML = "0.00";
+  });
+}
+
+
+
+initialiseStop();
+initialiseReset();
+//stopWatch()
--- stopwatchtests.js
+++ stopwatchtests.js
@@ -10,13 +10,13 @@ test('testing function is working', function(assert) {
   assert.equal(actual, expected, 'test environment is set up correctly')
 })
 
-test('start button injects response into results div', function(assert) {
+test('reset button injects response into results div', function(assert) {
   expect(0)
-  var start_button = document.getElementById("start_button")
-  document.getElementById("start_button").addEventListener("click", function(){
+  var reset_button = document.getElementById("reset_button")
+  document.getElementById("reset_button").addEventListener("click", function(){
     var actual = document.getElementById("results").innerHTML;
-    var expected = 'The start button works';
-    assert.equal(actual, expected, 'the start button injects')
+    var expected = '0.00';
+    assert.equal(actual, expected, 'the reset button injects')
   })
 })
 
@@ -27,25 +27,5 @@ test('stop button injects response into results div', function(assert) {
     var actual = document.getElementById("results").innerHTML;
     var expected = "The stop button works"
     assert.equal(actual, expected, 'the stop button injects')
-  })
-})
-
-test('reset button injects response into results div', function(assert) {
-  expect(0)
-  var reset_button = document.getElementById("reset_button")
-  document.getElementById("reset_button").addEventListener("click", function(){
-    var actual = document.getElementById("results").innerHTML;
-    var expected = "The reset button works"
-    assert.equal(actual, expected, 'the reset button injects')
-  })
-})
-
-test('start button triggers timer function which triggers increment function and changes result div to 1', function(assert) {
-  expect(0)
-  var start_button = document.getElementById("start_button")
-  document.getElementById("start_button").addEventListener("click", function(){
-    var actual = document.getElementById("results").innerHTML;
-    var expected = '0';
-    assert.equal(actual, expected, 'the start button makes the result div 1')
   })
 })
