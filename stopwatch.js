699cb283cacc3247d2055602c523f21c83ee3ff0
Heather <hdavidson94@gmail.com>
2016-06-15 10:22:06 +0100

    Add buttons

---
+++ stopwatch.js
@@ -0,0 +1,3 @@
+function stopWatch(time) {
+  return time;
+}
--- stopwatchtests.js
+++ stopwatchtests.js
@@ -3,3 +3,9 @@ test('testing the testing environment', function(assert) {
   var expected = 1
   assert.equal(actual, expected, 'test environment is set up correctly')
 })
+
+test('testing function is working', function(assert) {
+  var actual = 1
+  var expected = 1
+  assert.equal(1, 1, 'test environment is set up correctly')
+})
