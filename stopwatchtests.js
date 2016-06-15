721b17e7f8d22eb91160992defa03a4cdc6014c7
Heather <hdavidson94@gmail.com>
2016-06-15 16:58:49 +0100

    Improved start timer test

--- stopwatchtests.js
+++ stopwatchtests.js
@@ -4,19 +4,13 @@ test('testing the testing environment', function(assert) {
   assert.equal(actual, expected, 'test environment is set up correctly')
 })
 
-test('testing function is working', function(assert) {
-  var actual = 1
-  var expected = 1
-  assert.equal(actual, expected, 'test environment is set up correctly')
-})
-
 test('start timer runs', function(assert) {
     var done = assert.async();
     var start_button = document.getElementById("start_button")
       document.getElementById("start_button").addEventListener("click", function(){
-        var actual = document.getElementById("results").innerHTML;
-        var expected = '0.00';
-        assert.equal(actual, expected, 'the start timer runs')
+        var actual = tester;
+        var expected = true;
+        assert.equal(actual, expected, "the start timer runs")
         done();
       })
 })
@@ -26,8 +20,8 @@ test('reset button injects HTML into results div', function(assert) {
   var reset_button = document.getElementById("reset_button")
   document.getElementById("reset_button").addEventListener("click", function(){
     var actual = document.getElementById("results").innerHTML;
-    var expected = '0.00';
-    assert.equal(actual, expected, 'the reset button injects')
+    var expected = "0.00";
+    assert.equal(actual, expected, "the reset button injects")
     done();
   })
 })
