b183f362576208e91f8505016672140aba311126
Heather <hdavidson94@gmail.com>
2016-06-15 15:31:03 +0100

    Functioning stopwatch

--- stopwatchtests.js
+++ stopwatchtests.js
@@ -10,22 +10,12 @@ test('testing function is working', function(assert) {
   assert.equal(actual, expected, 'test environment is set up correctly')
 })
 
-test('reset button injects response into results div', function(assert) {
+test('reset button injects HTML into results div', function(assert) {
   expect(0)
   var reset_button = document.getElementById("reset_button")
   document.getElementById("reset_button").addEventListener("click", function(){
     var actual = document.getElementById("results").innerHTML;
     var expected = '0.00';
     assert.equal(actual, expected, 'the reset button injects')
-  })
-})
-
-test('stop button injects response into results div', function(assert) {
-  expect(0)
-  var stop_button = document.getElementById("stop_button")
-  document.getElementById("stop_button").addEventListener("click", function(){
-    var actual = document.getElementById("results").innerHTML;
-    var expected = "The stop button works"
-    assert.equal(actual, expected, 'the stop button injects')
   })
 })
