7d108bf76935902e17b6514e5d8dabcffac82388
Heather <hdavidson94@gmail.com>
2016-06-16 15:03:49 +0100

    added countdown buttons

--- stopwatchtests.js
+++ stopwatchtests.js
@@ -1,8 +1,8 @@
-var start_button = document.getElementById("start_button");
-var stop_button = document.getElementById("stop_button");
-var reset_button = document.getElementById("reset_button");
+var start_button = document.getElementById("stopwatch_start_button");
+var stop_button = document.getElementById("stopwatch_stop_button");
+var reset_button = document.getElementById("stopwatch_reset_button");
 
-var countdown_button = document.getElementById("countdown_button");
+var countdown_start_button = document.getElementById("countdown_start_button");
 
 
 test('testing the testing environment', function(assert) {
@@ -18,7 +18,7 @@ test('time is initialised to zero', function(assert) {
 
 test('start triggers timer', function(assert) {
   var done = assert.async();
-  start_button.click();
+  stopwatch_start_button.click();
 
   setTimeout(function() {
     var actual = document.getElementById('results').innerHTML;
@@ -29,10 +29,10 @@ test('start triggers timer', function(assert) {
 
 test('stop pauses timer', function(assert) {
   var done = assert.async();
-  start_button.click();
+  stopwatch_start_button.click();
 
   setTimeout(function() {
-    stop_button.click();
+    stopwatch_stop_button.click();
     var time1 = document.getElementById('results').innerHTML;
 
     setTimeout(function() {
@@ -46,7 +46,7 @@ test('stop pauses timer', function(assert) {
 
 test('reset sets a paused timer back to zero', function(assert) {
   document.getElementById('results').innerHTML = '9.45';
-  reset_button.click();
+  stopwatch_reset_button.click();
   var time = document.getElementById('results').innerHTML;
   assert.equal(time, 0.00);
 });
@@ -54,9 +54,9 @@ test('reset sets a paused timer back to zero', function(assert) {
 
 test('reset stops a running timer and sets it back to zero', function(assert) {
   var done = assert.async();
-  start_button.click();
+  stopwatch_start_button.click();
   setTimeout(function() {
-    reset_button.click();
+    stopwatch_reset_button.click();
     var time = document.getElementById('results').innerHTML;
     assert.equal(time, 0.00);
     done();
@@ -65,7 +65,7 @@ test('reset stops a running timer and sets it back to zero', function(assert) {
 
 test('countdown button starts countdown going', function(assert) {
   var done = assert.async();
-  countdown_button.click();
+  countdown_reset_button.click();
 
   setTimeout(function() {
     var time = document.getElementById('countdown_results').innerHTML;
@@ -76,10 +76,10 @@ test('countdown button starts countdown going', function(assert) {
 
 test('stop button stops countdown', function(assert) {
   var done = assert.async();
-  countdown_button.click();
+  countdown_start_button.click();
 
   setTimeout(function() {
-    stop_button.click();
+    countdown_stop_button.click();
     var time1 = document.getElementById('countdown_results').innerHTML;
 
     setTimeout(function() {
@@ -93,7 +93,7 @@ test('stop button stops countdown', function(assert) {
 
 test('reset button resets countdown timer', function(assert) {
   document.getElementById('countdown_results').innerHTML = '5.00';
-  reset_button.click();
+  countdown_reset_button.click();
   var time2 = document.getElementById('countdown_results').innerHTML;
   assert.equal(time2, 240.00, "gets set back to 240.00");
 });
